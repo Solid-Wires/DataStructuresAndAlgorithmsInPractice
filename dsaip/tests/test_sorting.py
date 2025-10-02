1fa5756147ef1290b6631f1183851d9e4702883e
Solid Wires <solid.wires@gmail.com>
2025-10-02 00:20:17 -0400

    Well, looks like it was because pytest-benchmark already calls and sorts the list from that function, therefore no additional tests can be ran consistently. I fixed this issue. Now the tests make a lot more sense now and align with what my professors were talking about.

--- dsaip/tests/test_sorting.py
+++ dsaip/tests/test_sorting.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from algorithms import bubble_sort, insertion_sort, merge_sort
 
 DATA_PATTERNS = Path(__file__).parent / "data/patterns.json"
+BENCHMARK_INPUT_SIZE: int = 2000
 
 def load_int_pattern(idx: int) -> dict:
     with DATA_PATTERNS.open('r') as file:
@@ -35,33 +36,11 @@ def test_merge_sort():
     print(arr, sep=',')
     assert arr == pattern["expected"]
 
-# Medium input tests
-MED_INPUT_PATTERN_IDX = 3
-
-@pytest.mark.skip(reason="Sample size not large enough to be interesting")
-def test_bubble_sort_large_n(benchmark):
-    pattern = load_int_pattern(MED_INPUT_PATTERN_IDX)
-    arr = pattern["initial"]
-    benchmark(bubble_sort, arr)
-    assert arr == pattern["expected"]
-
-@pytest.mark.skip(reason="Sample size not large enough to be interesting")
-def test_insertion_sort_large_n(benchmark):
-    pattern = load_int_pattern(MED_INPUT_PATTERN_IDX)
-    arr = pattern["initial"]
-    benchmark(insertion_sort, arr)
-    assert arr == pattern["expected"]
-
-@pytest.mark.skip(reason="Sample size not large enough to be interesting")
-def test_merge_sort_large_n(benchmark):
-    pattern = load_int_pattern(MED_INPUT_PATTERN_IDX)
-    arr = pattern["initial"]
-    benchmark(merge_sort, arr)
-    assert arr == pattern["expected"]
-
 # Huge, random input benchmarking tests
-def generate_random_input_size(size: int) -> dict:
-    sample_arr = random.sample(range(10000), size)
+
+# Helper function which generates fixed input sizes of random numbers
+def generate_random_input_size_n(size: int) -> dict:
+    sample_arr = random.sample(range(BENCHMARK_INPUT_SIZE * 100), size)
     sorted_arr = sample_arr.copy()
     sorted_arr.sort() # Using built-in sort
     return {
@@ -69,20 +48,24 @@ def generate_random_input_size(size: int) -> dict:
         "expected": sorted_arr
     }
 
-def test_bubble_sort_random_huge_n(benchmark):
-    pattern = generate_random_input_size(2000)
-    arr = pattern["initial"]
-    benchmark(bubble_sort, arr)
-    assert arr == pattern["expected"]
+# Helper function which sandboxes sort functions so that benchmark can study run cases
+def benchmark_run_sort_fn(fn: callable, initial: list[int]):
+    arr = initial.copy() # Otherwise, arr gets already sorted and benchmark is useless
+    fn(arr)
+    return arr
+    
+#@pytest.mark.skip(reason="Sample size was way too large to compute")
+def test_bubble_sort_random_ints_constant_n(benchmark):
+    pattern = generate_random_input_size_n(BENCHMARK_INPUT_SIZE)
+    assert benchmark(benchmark_run_sort_fn,
+        bubble_sort, pattern["initial"]) == pattern["expected"]
 
-def test_insertion_sort_random_huge_n(benchmark):
-    pattern = generate_random_input_size(2000)
-    arr = pattern["initial"]
-    benchmark(insertion_sort, arr)
-    assert arr == pattern["expected"]
+def test_insertion_sort_random_ints_constant_n(benchmark):
+    pattern = generate_random_input_size_n(BENCHMARK_INPUT_SIZE)
+    assert benchmark(benchmark_run_sort_fn,
+        insertion_sort, pattern["initial"]) == pattern["expected"]
 
-def test_merge_sort_random_huge_n(benchmark):
-    pattern = generate_random_input_size(2000)
-    arr = pattern["initial"]
-    benchmark(merge_sort, arr)
-    assert arr == pattern["expected"]
+def test_merge_sort_random_ints_constant_n(benchmark):
+    pattern = generate_random_input_size_n(BENCHMARK_INPUT_SIZE)
+    assert benchmark(benchmark_run_sort_fn,
+        merge_sort, pattern["initial"]) == pattern["expected"]
