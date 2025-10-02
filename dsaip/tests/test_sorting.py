52db2b7c8b51cab266df8b6adc8de7353ca49ee3
Solid Wires <solid.wires@gmail.com>
2025-10-02 00:43:11 -0400

    Accepted some feedback: removed duplicate correctness tests in favor of benchmark tests, made benchmark function call more readable

--- dsaip/tests/test_sorting.py
+++ dsaip/tests/test_sorting.py
@@ -8,35 +8,11 @@ from algorithms import bubble_sort, insertion_sort, merge_sort
 DATA_PATTERNS = Path(__file__).parent / "data/patterns.json"
 BENCHMARK_INPUT_SIZE: int = 2000
 
+# Helper function which loads integer list patterns for edge cases
 def load_int_pattern(idx: int) -> dict:
     with DATA_PATTERNS.open('r') as file:
         data = json.load(file)
     return data["integer_arrays"][idx]
-
-# Base tests
-
-def test_bubble_sort():
-    pattern = load_int_pattern(1)
-    arr = pattern["initial"]
-    bubble_sort(arr)
-    print(arr, sep=',')
-    assert arr == pattern["expected"]
-
-def test_insertion_sort():
-    pattern = load_int_pattern(0)
-    arr = pattern["initial"]
-    insertion_sort(arr)
-    print(arr, sep=',')
-    assert arr == pattern["expected"]
-
-def test_merge_sort():
-    pattern = load_int_pattern(2)
-    arr = pattern["initial"]
-    merge_sort(arr)
-    print(arr, sep=',')
-    assert arr == pattern["expected"]
-
-# Huge, random input benchmarking tests
 
 # Helper function which generates fixed input sizes of random numbers
 def generate_random_input_size_n(size: int) -> dict:
@@ -57,15 +33,15 @@ def benchmark_run_sort_fn(fn: callable, initial: list[int]):
 #@pytest.mark.skip(reason="Sample size was way too large to compute")
 def test_bubble_sort_random_ints_constant_n(benchmark):
     pattern = generate_random_input_size_n(BENCHMARK_INPUT_SIZE)
-    assert benchmark(benchmark_run_sort_fn,
-        bubble_sort, pattern["initial"]) == pattern["expected"]
+    result = benchmark(lambda: benchmark_run_sort_fn(bubble_sort, pattern["initial"]))
+    assert result == pattern["expected"]
 
 def test_insertion_sort_random_ints_constant_n(benchmark):
     pattern = generate_random_input_size_n(BENCHMARK_INPUT_SIZE)
-    assert benchmark(benchmark_run_sort_fn,
-        insertion_sort, pattern["initial"]) == pattern["expected"]
+    result = benchmark(lambda: benchmark_run_sort_fn(insertion_sort, pattern["initial"]))
+    assert result == pattern["expected"]
 
 def test_merge_sort_random_ints_constant_n(benchmark):
     pattern = generate_random_input_size_n(BENCHMARK_INPUT_SIZE)
-    assert benchmark(benchmark_run_sort_fn,
-        merge_sort, pattern["initial"]) == pattern["expected"]
+    result = benchmark(lambda: benchmark_run_sort_fn(merge_sort, pattern["initial"]))
+    assert result == pattern["expected"]
