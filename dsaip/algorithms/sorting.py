c814a6a5eed3650096c7a61081feabd83c9e5c41
Solid Wires <solid.wires@gmail.com>
2025-10-01 21:49:43 -0400

    Major refactoring of list to arr, primarily because I settled over the fact that there's too much type hinting confusion

--- dsaip/algorithms/sorting.py
+++ dsaip/algorithms/sorting.py
@@ -2,20 +2,20 @@
 # Sort an array using the infamous, naive bubble sort
 # O(n^2) complexity
 # https://www.geeksforgeeks.org/dsa/bubble-sort-algorithm/
-def bubble_sort(list: list[int]):
-    n = len(list)
+def bubble_sort(arr: list[int]):
+    n: int = len(arr)
     
     # Start from index 0 to size of list exclusive (n - 1)
     for i in range(n):
-        swapped = False
+        swapped: bool = False
         
         # Last i elements are already in place
         # Traverse the array from 0 to last i elements exclusive (n - i - 2)
         for j in range(0, n - i - 1):
             
             # Swap if this element is greater than the next element
-            if list[j] > list[j + 1]:
-                list[j], list[j + 1] = list[j + 1], list[j]
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
         # Get out of the loop if we didn't swap at all
         if swapped == False:
@@ -24,23 +24,34 @@ def bubble_sort(list: list[int]):
 # Sort an array using insertion sort
 # O(n) complexity
 # https://www.geeksforgeeks.org/dsa/insertion-sort-algorithm/
-def insertion_sort(list: list[int]):
-    n = len(list)
+def insertion_sort(arr: list[int]):
+    n: int = len(arr)
     
     # Start from index 1 to size of list exclusive (n - 1)
     for i in range(1, n):
-        cur: int = list[i]
-        j = i - 1
+        cur: int = arr[i]
+        j: int = i - 1
         
         # previous (j) must be greater than 0 and current must be less than the previous integer
         # If so, then we move this smaller integer backwards until it's in the right placement
         # e.g. [8, 12, 11] becomes [8, 11, 12] on last passthrough
-        while j >= 0 and cur < list[j]:
-            list[j + 1] = list[j]
+        while j >= 0 and cur < arr[j]:
+            arr[j + 1] = arr[j]
             j -= 1
-        list[j + 1] = cur
+        arr[j + 1] = cur
 
 # Sort an array using merge sort
 # O(nlog(n)) complexity
-def merge_sort(list: list[int]):
+# https://www.geeksforgeeks.org/dsa/merge-sort/
+# GeeksForGeeks' implementation is a little strange. This implementation is going to
+# be a lot more compact and gets straight to the point for the initial left and right values.
+def merge_sort(arr0: list[int]):
+    
+    # Merge two lists.
+    def merge(arr1: list[int], left: int, mid: int, right:int):
+        pass
+    
+    left: int = 0
+    right: int = len(arr0) - 1
+    
     raise NotImplementedError
--- dsaip/tests/test_sorting.py
+++ dsaip/tests/test_sorting.py
@@ -9,43 +9,51 @@ DATA_PATTERNS = Path(__file__).parent / "data/patterns.json"
 def load_int_pattern(idx: int):
     with DATA_PATTERNS.open('r') as file:
         data = json.load(file)
-    return data["integer_lists"][idx]
+    return data["integer_arrays"][idx]
 
 # Base tests
 
 def test_bubble_sort():
     pattern = load_int_pattern(1)
-    list = pattern["initial"]
-    bubble_sort(list)
-    print(list, sep=',')
-    assert list == pattern["expected"]
+    arr = pattern["initial"]
+    bubble_sort(arr)
+    print(arr, sep=',')
+    assert arr == pattern["expected"]
 
 def test_insertion_sort():
     pattern = load_int_pattern(0)
-    list = pattern["initial"]
-    insertion_sort(list)
-    print(list, sep=',')
-    assert list == pattern["expected"]
+    arr = pattern["initial"]
+    insertion_sort(arr)
+    print(arr, sep=',')
+    assert arr == pattern["expected"]
 
-# def test_merge_sort():
-#     pattern = load_int_pattern(0)
-#     list = pattern["initial"]
-#     merge_sort(list)
-#     print(list, sep=',')
-#     assert list == pattern["expected"]
+def test_merge_sort():
+    pattern = load_int_pattern(2)
+    arr = pattern["initial"]
+    merge_sort(arr)
+    print(arr, sep=',')
+    assert arr == pattern["expected"]
 
 # Large input tests
+LARGE_INPUT_PATTERN_IDX = 3
 
 def test_bubble_sort_large_n(benchmark):
-    pattern = load_int_pattern(2)
-    list = pattern["initial"]
-    benchmark(bubble_sort, list)
-    print(list, sep=',')
-    assert list == pattern["expected"]
+    pattern = load_int_pattern(LARGE_INPUT_PATTERN_IDX)
+    arr = pattern["initial"]
+    benchmark(bubble_sort, arr)
+    print(arr, sep=',')
+    assert arr == pattern["expected"]
 
 def test_insertion_sort_large_n(benchmark):
-    pattern = load_int_pattern(2)
-    list = pattern["initial"]
-    benchmark(insertion_sort, list)
-    print(list, sep=',')
-    assert list == pattern["expected"]
+    pattern = load_int_pattern(LARGE_INPUT_PATTERN_IDX)
+    arr = pattern["initial"]
+    benchmark(insertion_sort, arr)
+    print(arr, sep=',')
+    assert arr == pattern["expected"]
+
+def test_merge_sort_large_n(benchmark):
+    pattern = load_int_pattern(LARGE_INPUT_PATTERN_IDX)
+    arr = pattern["initial"]
+    benchmark(merge_sort, arr)
+    print(arr, sep=',')
+    assert arr == pattern["expected"]
