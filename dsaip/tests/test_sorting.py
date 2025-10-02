0f8ea96d15d3a2a6867e521324a8656dc7c815ce
Solid Wires <solid.wires@gmail.com>
2025-10-02 00:52:07 -0400

    Integrated some more feedback: now uses @pytest.mark.parametrize for individual scaling tests. Cleaned up sorted_arr. Updated analysis with better looking benchmark test information.

--- dsaip/tests/test_sorting.py
+++ dsaip/tests/test_sorting.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from algorithms import bubble_sort, insertion_sort, merge_sort
 
 DATA_PATTERNS = Path(__file__).parent / "data/patterns.json"
-BENCHMARK_INPUT_SIZE: int = 2000
+random.seed(42) # For reproducability
 
 # Helper function which loads integer list patterns for edge cases
 def load_int_pattern(idx: int) -> dict:
@@ -16,9 +16,8 @@ def load_int_pattern(idx: int) -> dict:
 
 # Helper function which generates fixed input sizes of random numbers
 def generate_random_input_size_n(size: int) -> dict:
-    sample_arr = random.sample(range(BENCHMARK_INPUT_SIZE * 100), size)
-    sorted_arr = sample_arr.copy()
-    sorted_arr.sort() # Using built-in sort
+    sample_arr = random.sample(range(20000), size)
+    sorted_arr = sorted(sample_arr) # Using built-in sort
     return {
         "initial": sample_arr,
         "expected": sorted_arr
@@ -30,18 +29,20 @@ def benchmark_run_sort_fn(fn: callable, initial: list[int]):
     fn(arr)
     return arr
     
-#@pytest.mark.skip(reason="Sample size was way too large to compute")
-def test_bubble_sort_random_ints_constant_n(benchmark):
-    pattern = generate_random_input_size_n(BENCHMARK_INPUT_SIZE)
+@pytest.mark.parametrize("n", [20, 200, 2000])
+def test_bubble_sort_scaling(benchmark, n: int):
+    pattern = generate_random_input_size_n(n)
     result = benchmark(lambda: benchmark_run_sort_fn(bubble_sort, pattern["initial"]))
     assert result == pattern["expected"]
 
-def test_insertion_sort_random_ints_constant_n(benchmark):
-    pattern = generate_random_input_size_n(BENCHMARK_INPUT_SIZE)
+@pytest.mark.parametrize("n", [20, 200, 2000])
+def test_insertion_sort_scaling(benchmark, n: int):
+    pattern = generate_random_input_size_n(n)
     result = benchmark(lambda: benchmark_run_sort_fn(insertion_sort, pattern["initial"]))
     assert result == pattern["expected"]
 
-def test_merge_sort_random_ints_constant_n(benchmark):
-    pattern = generate_random_input_size_n(BENCHMARK_INPUT_SIZE)
+@pytest.mark.parametrize("n", [20, 200, 2000])
+def test_merge_sort_scaling(benchmark, n: int):
+    pattern = generate_random_input_size_n(n)
     result = benchmark(lambda: benchmark_run_sort_fn(merge_sort, pattern["initial"]))
     assert result == pattern["expected"]
