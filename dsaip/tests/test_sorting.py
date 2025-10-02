ef4d4af41f75947655119f61242cce3b2f0250d3
Solid-Wires <solid.wires@gmail.com>
2025-10-02 12:24:36 -0400

    Moved some pattern helper functions into their own file for organization

--- dsaip/tests/test_sorting.py
+++ dsaip/tests/test_sorting.py
@@ -1,27 +1,7 @@
 import pytest
 import pytest_benchmark
-from json import load as json_load
-from random import sample
-from pathlib import Path
 from algorithms import bubble_sort, insertion_sort, merge_sort
-
-# File path for data patterns
-DATA_PATTERNS: Path = Path(__file__).parent / "data/patterns.json"
-
-# Helper function which loads integer list patterns for edge cases
-def load_int_pattern(idx: int) -> dict:
-    with DATA_PATTERNS.open('r') as file:
-        data = json_load(file)
-    return data["integer_arrays_sorting"][idx]
-
-# Helper function which generates fixed input sizes of random numbers
-def generate_random_input_size_n(size: int) -> dict:
-    sample_arr: list[int] = sample(range(20000), size)
-    sorted_arr: list[int] = sorted(sample_arr) # Using built-in sort
-    return {
-        "initial": sample_arr,
-        "expected": sorted_arr
-    }
+from .pattern_util import generate_random_input_size_n
 
 # Helper function which sandboxes sort functions so that benchmark can study run cases
 def benchmark_run_sort_fn(fn: callable, initial: list[int]) -> list[int]:
