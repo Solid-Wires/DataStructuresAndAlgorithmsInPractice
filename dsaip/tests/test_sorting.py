35b448028fdaf515af957daa1dff9b9c26fae648
Solid Wires <solid.wires@gmail.com>
2025-10-01 23:13:24 -0400

    Well, forgot that merge sort is O(nlogn) in time complexity, which means that it shouldn't be any better than insertion sort, so I am in fact sane. Random huge tests were added. Sorting algorithms completed and studied.

--- dsaip/tests/test_sorting.py
+++ dsaip/tests/test_sorting.py
@@ -1,12 +1,13 @@
 import pytest
 import pytest_benchmark
 import json
+import random
 from pathlib import Path
 from algorithms import bubble_sort, insertion_sort, merge_sort
 
 DATA_PATTERNS = Path(__file__).parent / "data/patterns.json"
 
-def load_int_pattern(idx: int):
+def load_int_pattern(idx: int) -> dict:
     with DATA_PATTERNS.open('r') as file:
         data = json.load(file)
     return data["integer_arrays"][idx]
@@ -34,26 +35,54 @@ def test_merge_sort():
     print(arr, sep=',')
     assert arr == pattern["expected"]
 
-# Large input tests
-LARGE_INPUT_PATTERN_IDX = 3
+# Medium input tests
+MED_INPUT_PATTERN_IDX = 3
 
+@pytest.mark.skip(reason="Sample size not large enough to be interesting")
 def test_bubble_sort_large_n(benchmark):
-    pattern = load_int_pattern(LARGE_INPUT_PATTERN_IDX)
+    pattern = load_int_pattern(MED_INPUT_PATTERN_IDX)
     arr = pattern["initial"]
     benchmark(bubble_sort, arr)
-    print(arr, sep=',')
     assert arr == pattern["expected"]
 
+@pytest.mark.skip(reason="Sample size not large enough to be interesting")
 def test_insertion_sort_large_n(benchmark):
-    pattern = load_int_pattern(LARGE_INPUT_PATTERN_IDX)
+    pattern = load_int_pattern(MED_INPUT_PATTERN_IDX)
     arr = pattern["initial"]
     benchmark(insertion_sort, arr)
-    print(arr, sep=',')
     assert arr == pattern["expected"]
 
+@pytest.mark.skip(reason="Sample size not large enough to be interesting")
 def test_merge_sort_large_n(benchmark):
-    pattern = load_int_pattern(LARGE_INPUT_PATTERN_IDX)
+    pattern = load_int_pattern(MED_INPUT_PATTERN_IDX)
     arr = pattern["initial"]
     benchmark(merge_sort, arr)
-    print(arr, sep=',')
+    assert arr == pattern["expected"]
+
+# Huge, random input benchmarking tests
+def generate_random_input_size(size: int) -> dict:
+    sample_arr = random.sample(range(10000), size)
+    sorted_arr = sample_arr.copy()
+    sorted_arr.sort() # Using built-in sort
+    return {
+        "initial": sample_arr,
+        "expected": sorted_arr
+    }
+
+def test_bubble_sort_random_huge_n(benchmark):
+    pattern = generate_random_input_size(2000)
+    arr = pattern["initial"]
+    benchmark(bubble_sort, arr)
+    assert arr == pattern["expected"]
+
+def test_insertion_sort_random_huge_n(benchmark):
+    pattern = generate_random_input_size(2000)
+    arr = pattern["initial"]
+    benchmark(insertion_sort, arr)
+    assert arr == pattern["expected"]
+
+def test_merge_sort_random_huge_n(benchmark):
+    pattern = generate_random_input_size(2000)
+    arr = pattern["initial"]
+    benchmark(merge_sort, arr)
     assert arr == pattern["expected"]
