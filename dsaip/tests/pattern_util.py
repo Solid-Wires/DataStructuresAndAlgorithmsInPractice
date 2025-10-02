c80f74cc5c0386817065025b767750a44e5ea687
Solid-Wires <solid.wires@gmail.com>
2025-10-02 12:47:56 -0400

    Some refactoring

--- dsaip/tests/pattern_util.py
+++ dsaip/tests/pattern_util.py
@@ -8,14 +8,14 @@ from random import sample
 DATA_PATTERNS: Path = Path(__file__).parent / "data/patterns.json"
 
 # Loads integer list patterns for edge cases
-def load_int_pattern(idx: int) -> dict:
+def load_int_arr_sorting_pattern(idx: int) -> dict:
     with DATA_PATTERNS.open('r') as file:
         data = json_load(file)
     return data["integer_arrays_sorting"][idx]
 
 # Generates fixed input sizes of random number patterns
-def generate_random_input_size_n(size: int) -> dict:
-    sample_arr: list[int] = sample(range(20000), size)
+def generate_random_input_size(n: int) -> dict:
+    sample_arr: list[int] = sample(range(20000), n)
     sorted_arr: list[int] = sorted(sample_arr) # Using built-in sort
     return {
         "initial": sample_arr,
--- dsaip/tests/test_insertion_sort.py
+++ dsaip/tests/test_insertion_sort.py
@@ -1,29 +1,30 @@
 import pytest
 from algorithms import insertion_sort
 from .benchmark_util import benchmark_run_sort_fn as run_sort_fn
-from .pattern_util import generate_random_input_size_n, load_int_pattern
+from .pattern_util import generate_random_input_size
+from .pattern_util import load_int_arr_sorting_pattern as load_pattern
 
 # Scaling + Functionality
 
 @pytest.mark.parametrize("n", [20, 200, 2000])
 def test_insertion_sort_scaling(benchmark, n: int):
-    pattern: dict = generate_random_input_size_n(n)
+    pattern: dict = generate_random_input_size(n)
     result: list[int] = benchmark(lambda: run_sort_fn(insertion_sort, pattern["initial"]))
     assert result == pattern["expected"]
 
 # Edge case patterns
 
 def test_insertion_sort_negative_integers():
-    pattern: dict = load_int_pattern(3)
+    pattern: dict = load_pattern(3)
     result: list[int] = run_sort_fn(insertion_sort, pattern["initial"])
     assert result == pattern["expected"]
 
 def test_insertion_sort_contains_zero():
-    pattern: dict = load_int_pattern(4)
+    pattern: dict = load_pattern(4)
     result: list[int] = run_sort_fn(insertion_sort, pattern["initial"])
     assert result == pattern["expected"]
 
 def test_insertion_sort_duplicate_values():
-    pattern: dict = load_int_pattern(5)
+    pattern: dict = load_pattern(5)
     result: list[int] = run_sort_fn(insertion_sort, pattern["initial"])
     assert result == pattern["expected"]
